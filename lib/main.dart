4ab599f7f92c3c500f3214e60570541185753708
Eduardo Soprana Coelho <dudatimbe@gmail.com>
2022-03-24 14:52:02 -0300

    feat: themes

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,18 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          color: Colors.purple,
+        ),
+        primaryColor: Colors.green[900],
+        // ignore: deprecated_member_use
+        accentColor: Colors.redAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.accent,
+        ),
+      ),
       home: TransferList(),
     );
   }
